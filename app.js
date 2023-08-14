c9f5eb51c9d6f8d30825e6353f2e3415f06f6d93
vainkatesh.kashyap <vainkatesh.kashyap@servicenow.com>
2023-08-15 01:47:22 +0530

    episode-1

---
+++ app.js
@@ -0,0 +1,22 @@
+
+//React code 
+var temp=React.createElement("div",{id:"test1"},
+         React.createElement("div",{id:"test2"},
+        [React.createElement("h1",{},"Welcome Vainky to React"),
+        React.createElement("h2",{},"Welcome Vainky to React h2")]));
+
+// To Pass multiple elemenest in children use array just like above h1 and h2
+
+
+
+    
+var root=ReactDOM.createRoot(document.getElementById("parent"));
+root.render(temp);
+
+//Pure JS code
+// var t=document.createElement("h1");
+// t.innerHTML="Hello World in JS";
+
+// var test=document.getElementById("root");
+// test.appendChild(t);
+
