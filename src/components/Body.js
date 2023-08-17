3d4dde38a6351edb847c2867ac2b0168158ac8b5
vainkatesh.kashyap <vainkatesh.kashyap@servicenow.com>
2023-08-17 06:45:34 +0530

    episode-6

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,30 +1,67 @@
-import React,{ useState } from "react";
+import React,{ useState,useEffect } from "react";
 import RestaurentCard from "./RestaurentCard";
-import { restaurants } from "../utils/mockData";
+// import { restaurants } from "../utils/mockData";
+import Shimmer from "./Shimmer";
 import "../../index.css";
 
 const Body = () =>{
 
-    const [restaurantData,setRestaurantData]=useState(restaurants);
+    const [restaurantData,setRestaurantData]=useState([]);
+    const [filteredRestaurants,setFilteredRestaurants]=useState([]);
+
+    const [searchtext,setSearchText]=useState("");
+
+    useEffect(()=>{
+        fetchData("https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.439852&lng=78.383729&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING",4);
+    },[]);
+        
+
+    const fetchData= async (url,index) =>{
+        const data=await fetch(url);
+        const json=await data.json();
+
+        console.log(json);
+        // const newResData=[...restaurantData,...json?.data?.cards?.[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants]
+        
+        setRestaurantData(json?.data?.cards?.[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+        setFilteredRestaurants(json?.data?.cards?.[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    }
 
     const filterTopRatedRestaurents=()=>{
         const filteredRestaurents=restaurantData.filter(res=> res.info.avgRating>=4);
-        setRestaurantData(filteredRestaurents);
+        setFilteredRestaurants(filteredRestaurents);
     }
+
+    const showMoreRestaurents=()=>{
+        //fetchData("https://www.swiggy.com/dapi/restaurants/list/update?lat=17.439852&lng=78.383729&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING",0);
+    }
+
+    const search=()=>{
+        console.log(restaurantData);
+        const filteredData=restaurantData.filter(res=>res?.info?.name?.toLowerCase()?.includes(searchtext.toLowerCase()));
+        setFilteredRestaurants(filteredData);
+    }
+
+//Shimmer UI
+// if(restaurantData.length===0){
+//     return <Shimmer/>
+// }
 
     return (
             <div className="body">
                     <div className="search-container">
-                            <input className="search-input"/>
-                            <button className="search-button">Search</button>
+                            <input className="search-input" value={searchtext} onChange={(e)=>setSearchText(e.target.value)} />
+                            <button className="search-button" onClick={search}>Search</button>
                             <button className="top-rated-res" onClick={filterTopRatedRestaurents}>Top Rated Restaurents</button>
-                            <button className="top-rated-res" onClick={()=>setRestaurantData(restaurants)}>Show All Restaurents</button>
+                            <button className="all-res" onClick={()=>setFilteredRestaurants(restaurantData)}>Show All Restaurents</button>
+                            <button className="show-more" onClick={showMoreRestaurents}>Show More Restaurents</button>
                     </div>
-                    <div className="res-container">
-                            {restaurantData.map(restaurant=><RestaurentCard key={restaurant?.info?.id} resData={restaurant}/>)}
+                    {filteredRestaurants.length===0?<Shimmer/>:
+                    (<div className="res-container">
+                            {filteredRestaurants.map(restaurant=><RestaurentCard key={restaurant?.info?.id} resData={restaurant}/>)}
                             {/* <RestaurentCard resData={restaurants[0]}/>
                             <RestaurentCard resData={restaurants[1]}/> */}
-                    </div>
+                    </div>)}
             </div>
     );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,24 @@
 import { LOGO_URL } from "../utils/constants";
+import { useState } from "react";
 
-const Header=()=>(
+const Header=()=>{
+    const [btnName,setBtnName]=useState("Login");
+    return (
     <div className="header">
             <div className="logo-container">
                     <img className="logo" src={LOGO_URL}/>
             </div>
             <div className="nav-items">
                     <ul>
-                            <li>Home</li>
-                            <li>About us</li>
-                            <li>Contact us</li>
-                            <li>Cart</li>
+                            <li key="Home">Home</li>
+                            <li key="aboutus">About us</li>
+                            <li key="contactus">Contact us</li>
+                            <li key="cart">Cart</li>
+                            <li key="log" onClick={()=>btnName==="Login"?setBtnName("Logout"):setBtnName("Login")}>{btnName}</li>
                     </ul>
             </div>
     </div>
 );
+}
 
 export default Header;
